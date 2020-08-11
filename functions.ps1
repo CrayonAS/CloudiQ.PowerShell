e505877d865997f735dd8986bd973c817564cd27
Roberth Strand <me@robstr.dev>
2020-08-11 10:14:16 +0200

    Temporary files, development

---
+++ functions.ps1
@@ -0,0 +1,47 @@
+function Connect-CloudIQ {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $ClientId,
+        [Parameter()]
+        [string]
+        $ClientSecret
+    )
+    
+    $apiBaseUrl = 'https://api.crayon.com/api/v1'
+    
+    $headers = @{}
+    $encodedClientId = [System.Web.HttpUtility]::UrlEncode($clientId) 
+    $encodedSecret = [System.Web.HttpUtility]::UrlEncode($clientSecret) 
+    $credentials = "$($encodedClientId):$($encodedSecret)"
+    $Bytes = [System.Text.Encoding]::UTF8.GetBytes($credentials)
+    $EncodedText =[Convert]::ToBase64String($Bytes)
+    
+    $username = Read-Host -Prompt "Username"
+    $password = Read-Host -Prompt "Password" -AsSecureString
+    
+    $Body = @{
+        'username'=$username
+        'password'=$password | ConvertFrom-SecureString -AsPlainText
+        'grant_type'="password"
+        'scope'="CustomerApi"
+    }
+    
+    $headers.Add("Authorization", "Basic "+$EncodedText)
+    $headers.Add("Content-Type", "application/x-www-form-urlencoded")
+    
+    $OAuthReq = Invoke-RestMethod -Method Post -Uri $apiBaseUrl/connect/token -Body $Body -Headers $headers
+    
+    New-Variable -Name tokenType -Value $OAuthReq.tokentype -Scope Global -Force
+    New-Variable -Name AccessToken -Value $OAuthReq.accesstoken -Scope Global -Force
+}
+function Get-CloudIQSubscription {
+    [CmdletBinding()]
+    param (
+        [Parameter()]
+        [string]
+        $Organization
+    )
+    Invoke-RestMethod -Method Get -Uri "https://api.crayon.com/api/v1/subscriptions/?organizationID=$Organization" -Authentication $TokenType -Token (ConvertTo-SecureString -String $AccessToken -AsPlainText) | Select-Object -ExpandProperty Items
+}
