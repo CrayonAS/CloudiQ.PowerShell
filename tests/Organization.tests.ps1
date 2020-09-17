ff6e08edbfa3d43736d611f788fcab4f5fd2964d
Roberth Strand <me@robstr.dev>
2020-09-18 00:30:20 +0200

    Changed name of folder, and module name

--- tests/Organization.tests.ps1
+++ tests/Organization.tests.ps1
@@ -1,15 +1,16 @@
 BeforeAll {
     # Import module
-    Import-Module -Name "$PSScriptRoot/../module/Cloud-iQ.PowerShell.psd1" -Force -ErrorAction Stop
+    Import-Module -Name "$PSScriptRoot/../CloudiQ/CloudiQ.psd1" -Force -ErrorAction Stop
     Get-Content -Path "$PSScriptRoot/_mocks/organizations.ps1"
 }
 
 Describe 'Test Get-CloudiQOrganization' {
+    BeforeAll {
+        . "$PSScriptRoot/_mocks/organizations.ps1"
+    }
+
     It 'Should result in listing four demo customers' {
-        
-
         $Organizations = Get-CloudiQOrganization
-
         $Organizations.count | Should -be 4
     }
 }
